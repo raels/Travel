badbe81d995ebaeeab5057e7b9a4e4066f6c7895
Raels Koder <raels@uniblab.com>
2012-05-29 21:52:09 -0700

    Rails 3.2.3 installed

---
+++ travel/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = '7d2abdfeeac25014c099bfdbd8d1d54cad3ae4f2dc39a2c67a153c3c84ce4985f1daac820a37ea17ec2dfa5b73e8234cfcea0da9af8667968d0417e81158d967'
